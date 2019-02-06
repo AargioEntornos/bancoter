45a02e7bea0d3279bb96151712957abd9c444f88
AargioEntornos <otracuentadeosu2@gmail.com>
2019-02-06 09:58:52 +0100

    Primera modificación [terminal]

--- Banco.java
+++ Banco.java
@@ -1,5 +1,6 @@
 public class Banco {
     public static void main(String[] args) {
         System.out.println("Proyecto Banco [terminal]");
+	System.out.println("Primera modificación [terminal]");
     }
 }
